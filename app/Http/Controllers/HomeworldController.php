b1c421473e0b66b7a117dadbde0c914e435041a0
NazarSm <69159497+NazarSm@users.noreply.github.com>
2020-11-09 12:11:27 +0200

    created migration

---
+++ app/Http/Controllers/HomeworldController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Homeworld;
+use Illuminate\Http\Request;
+
+class HomeworldController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Homeworld  $homeworld
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Homeworld $homeworld)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Homeworld  $homeworld
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Homeworld $homeworld)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Homeworld  $homeworld
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Homeworld $homeworld)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Homeworld  $homeworld
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Homeworld $homeworld)
+    {
+        //
+    }
+}
